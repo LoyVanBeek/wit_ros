3bb6817c85d8b6f9b57d201f9a5835f943b4686f
Loy van Beek <loy.vanbeek@gmail.com>
2013-09-15 16:44:37 +0200

    Node represents outcome using the message

--- src/wit_ros/wit_ros.py
+++ src/wit_ros/wit_ros.py
@@ -10,15 +10,19 @@ import rospy
 import requests
 
 from wit_ros.srv import Interpret, InterpretResponse
+from wit_ros.msg import Outcome
 
 def interpret(rosrequest):
     httpresponse = requests.get('https://api.wit.ai/message?q={sentence}'.format(sentence=rosrequest.sentence), 
         headers={"Authorization":"Bearer {key}".format(key=APIKEY)})
     data = httpresponse.json()
     
+    outcome = Outcome(          confidence  =float(data["outcome"]["confidence"]), 
+                                intent      =str(data["outcome"]["intent"]))
+
     return InterpretResponse(   msg_body    = str(data["msg_body"]),
                                 msg_id      = str(data["msg_id"]),
-                                outcome     = str(data["outcome"]))
+                                outcome     = outcome)
 
 if __name__ == "__main__":
     rospy.init_node("wit_ros")
